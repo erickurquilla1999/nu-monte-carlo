405cacfd556a5a60694f09e7b84b9aa593b825da
erickurquilla1999 <erickurquilla1999@gmail.com>
2025-04-28 21:56:44 -0400

    Moving neutrino struct to particle interaction files for better organization

--- particle_interactions.h
+++ particle_interactions.h
@@ -6,6 +6,17 @@
 
 #include "particle_container.h"
 
+struct NeutrinoData
+{
+    enum {
+        n_invcm3,
+        fx_invcm3,
+        fy_invcm3,
+        fz_invcm3,
+        ncomps
+    };
+};
+
 void MoveParticlesMC(MCParticleContainer& particles, const amrex::MultiFab& state, const amrex::Geometry& geom, const amrex::Real dt);
 
 void compute_nu_n_and_f(MCParticleContainer& particles, const amrex::Geometry& geom, amrex::MultiFab& nu_n_and_f);
